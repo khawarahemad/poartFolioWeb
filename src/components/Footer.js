df5d90cc665c428aabfdf7887b30e66f98e4e9ca
theonehack <111216965+theonehack@users.noreply.github.com>
2025-01-25 00:34:35 +0530

    added GitHub Link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import logo from "../assets/img/logo1.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from "../assets/img/github1.svg";
 
 export const Footer = () => {
   return (
@@ -19,6 +20,7 @@ export const Footer = () => {
               <a href="https://www.linkedin.com/in/khawarahemad"><img src={navIcon1} alt="Li" /></a>
               <a href="https://www.facebook.com/khawar.ahemad.1"><img src={navIcon2} alt="Fb" /></a>
               <a href="https://www.instagram.com/khawar_ahemad"><img src={navIcon3} alt="In" /></a>
+              <a href="https://www.github.com/khawar_ahemad"><img src={navIcon4} alt="Git" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ import logo from '../assets/img/logo3.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIcon4 from '../assets/img/github1.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -53,6 +54,7 @@ export const NavBar = () => {
                 <a href="https://www.linkedin.com/in/khawarahemad"><img src={navIcon1} alt="Li" /></a>
                 <a href="https://www.facebook.com/khawar.ahemad.1"><img src={navIcon2} alt="Fb" /></a>
                 <a href="https://www.instagram.com/khawar_ahemad"><img src={navIcon3} alt="In" /></a>
+                <a href="https://www.github.com/khawar_ahemad"><img src={navIcon4} alt="Git" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
